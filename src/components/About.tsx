e447acf1fcecaf980e29a1fe4c88fb16c405d8ef
unnihari2332-cmd <unnihari2332@gmail.com>
2025-12-16 13:47:55 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -139,6 +139,7 @@ const CoreValues = () => {
             </p>
           </div>
 
+
           {/* Feature List */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-24 max-w-7xl mx-auto">
             {[
