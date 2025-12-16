fdb682ff40263ebd1de47884a4225fbc9c855bbd
unnihari2332-cmd <unnihari2332@gmail.com>
2025-12-16 11:44:01 +0530

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,5 +1,4 @@
 import Header from '@/components/Header';
-import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Services from '@/components/Services';
 import Footer from '@/components/Footer';
@@ -9,7 +8,6 @@ const Index = () => {
     <div className="min-h-screen bg-background">
       <Header />
       <main>
-        <Hero />
         <Services />
   
       </main>
