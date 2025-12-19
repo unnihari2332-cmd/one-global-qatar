356fb1c7c3187c5f2182940c2f240462fddfa8d0
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-12-19 19:27:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Services from "./pages/Services";
+import GlobalPresence from "./pages/GlobalPresence";
 
 const queryClient = new QueryClient();
 
@@ -20,7 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
-  
+          <Route path="/global-presence" element={<GlobalPresence />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
